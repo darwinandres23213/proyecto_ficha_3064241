abcc6e4ea14d9a0d2b127be932ccbafd2a23987c
Darwin <darwinandresgomez3@gmail.com>
2026-08-04 20:37:00 -0500

    correcciones v2

--- database/migrations/15_create_promociones_table.php
+++ database/migrations/15_create_promociones_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('promociones_table', function (Blueprint $table) {
+        Schema::create('promociones', function (Blueprint $table) {
             $table->id();
             $table->string("nombre",120)->nullable();
             $table->text("descripcion")->nullable();
