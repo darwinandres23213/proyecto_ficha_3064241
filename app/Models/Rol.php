2d573c8e4721dd70b65ab7b21b5aa0b8b48df574
Darwin <darwinandresgomez3@gmail.com>
2026-08-05 20:46:17 -0500

    commit 3

---
+++ app/Models/Rol.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Rol extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table =  "roles";
+
+    protected $fillable = [
+    'nombre_rol',
+    'descripcion',
+    'estado',
+    ];
+
+    public function usuario()
+    {
+        return $this->hasMany(Usuario::class);
+    }
+
+
+
+}
