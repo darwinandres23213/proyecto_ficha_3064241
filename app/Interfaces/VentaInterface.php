de1535d641cd237ee7177725408e2adb58ba2b2f
DylanM <dylanleonmoreno3@gmail.com>
2026-08-12 19:56:10 -0500

    creacion interfaz

--- app/Interfaces/VentaInterface.php
+++ app/Interfaces/VentaInterface.php
@@ -8,3 +8,6 @@ interface VentaInterface
     public function getById(int $id); // Obtener venta por id
     public function getByEstado(string $estado); // Filtrar ventas por estado: abierta | pagada | anulada
 }
+
+//hola
+
