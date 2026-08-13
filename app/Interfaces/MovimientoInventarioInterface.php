bafabeb5fe870170ff0cfafe1acaa3df914ad130
miguelcruz201715-hue <miguelcruz201715@gmail.com>
2026-08-12 20:22:41 -0500

    si

--- app/Interfaces/MovimientoInventarioInterface.php
+++ app/Interfaces/MovimientoInventarioInterface.php
@@ -4,9 +4,7 @@ namespace App\Interfaces;
 
 interface MovimientoInventarioInterface extends BaseInterface 
 {
-
     public function existeStockSuficiente(int $productoId, float $cantidad); 
     public function calcularValorTotalInventario(int $almacenId); 
     public function generarReporteMovimientos(string $fechaInicio, string $fechaFin); 
-    
 }
