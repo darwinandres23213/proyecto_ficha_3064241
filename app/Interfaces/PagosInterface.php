623bcb7c459e819b357315f7f0ed2f5ec42747f0
khanon21 <sebastiantriana06@gmail.com>
2026-08-12 19:56:36 -0500

    Remove return types from PagoInterface methods

--- app/Interfaces/PagosInterface.php
+++ app/Interfaces/PagosInterface.php
@@ -6,11 +6,11 @@ use DateTime;
 
 interface PagoInterface extends BaseInterface
 {
-    public function getByFecha(DateTime $fecha_pago): ?array;
+    public function getByFecha(DateTime $fecha_pago);
 
-    public function TieneReferencia(?string $referencia): bool;
+    public function TieneReferencia(?string $referencia);
 
-    public function ObtenerEstado(int $pago_id): string;
+    public function ObtenerEstado(int $pago_id);
 
-    public function VerificacionDePago(array $detalledepago): bool;
+    public function VerificacionDePago(array $detalledepago);
 }
