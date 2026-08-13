8b64ebf1ee438620e93f8d7a212d8dfd0283b95a
Anderson Sanchez <anderson.asm201@gmail.com>
2026-08-12 20:14:59 -0500

    cambios en interfaz orden de compra

--- app/Interfaces/OrdenCompra.php
+++ app/Interfaces/OrdenCompra.php
@@ -4,9 +4,8 @@ namespace App\Interfaces;
 
 interface OrdenCompra extends BaseInterface
 {
-  public function findbyNumeroOrden(string $numeroOrden);
-  public function findbyEstado(string $estado);
-
+  public function findByNumeroOrden(string $numeroOrden);
+  public function findByEstado(string $estado);
   public function findByFecha(datatime $fecha);
 
 }
