0b42dd987faccb21054367a52796c5b971df8adc
Darwin <darwinandresgomez3@gmail.com>
2026-08-26 15:59:13 -0500

    version1

--- app/Interfaces/PromocionInterface.php
+++ app/Interfaces/PromocionInterface.php
@@ -4,7 +4,7 @@ namespace App\Interfaces;
 
 interface PromocionInterface extends BaseInterface
 {
-    public function findByStatusPromocion(boolean $statusPromocion);
+    public function findByStatusPromocion(bool $statusPromocion);
     public function finByFechaInicio (datetime $fechaInicio);
     public function aplicarPromocion(int $promocionId, float $precio);
 }
---
+++ app/Repository/BaseRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository;
+
+use App\Interfaces\BaseInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseInterface
+{
+   protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function create(array $datos)
+    {
+        return $this->model->create($datos);
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function getById(int $id)
+    {
+        $registro = $this->model->find($id);
+
+        if(!$registro)
+        {
+            return null;
+        }
+
+        return $registro;
+    }
+
+    public function update(array $datos, int $id)
+    {
+
+        $registro = $this->model->find($id);
+
+        if(!$registro)
+        {
+            return null;
+        }               
+        
+        $registro->update($datos);
+
+       return $registro->fresh();
+    }
+
+    public function delete(int $id)
+    {
+        $registro = $this->model->find($id);
+
+        if(!$registro)
+        {
+            return null;
+        }               
+        
+        return $this->model->delete($id);
+    }
+
+
+}
