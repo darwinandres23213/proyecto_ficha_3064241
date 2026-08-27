f19ee21b78a0ab55ff0693ce8acbfbfe85999a51
Anderson Sanchez <anderson.asm201@gmail.com>
2026-08-26 21:04:59 -0500

    creacion de funciones

---
+++ app/Repository/ZonaRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repository;
+
+
+use App\Interfaces\ZonaInterface;
+use App\Models\Zona;
+
+class ZonaRepository extends BaseRepository implements ZonaInterface
+
+{
+    public function __construct(Zona $Zona1)
+    {
+        parent::__construct($Zona1);
+    }
+
+    public function find($id)
+    
+    {
+        return $this->model->find($id);
+    }
+
+    public function findByAforoMaximo($aforoMaximo)
+    {
+        return $this->model->where('aforo_maximo', $aforoMaximo)->get();
+    }
+
+    public function findByNombreZona($nombreZona)
+    {
+        return $this->model->where('nombre_zona', $nombreZona)->get();
+    }
+
+    public function findByPrecioCover($precioCover)
+    {
+        return $this->model->where('precio_cover', $precioCover)->get();
+    }
+}   
