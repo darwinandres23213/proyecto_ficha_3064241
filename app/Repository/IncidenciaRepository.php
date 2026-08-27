8b963f995e129e78be430da31f1d8d3de2e1e42f
SENA <dylanleonmoreno3@gmail.com>
2026-08-26 20:53:15 -0500

    Se crea repositorio para incidencias

---
+++ app/Repository/IncidenciaRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repository;
+
+use App\Interfaces\IncidenciaInterface;
+use App\Models\Incidencia;
+
+class IncidenciaRepository extends BaseRepository implements IncidenciaInterface
+{
+    public function __construct(Incidencia $incidencia)
+    {
+        parent::__construct($incidencia);
+    }
+
+    public function getByEstado(string $estado)
+    {
+        return $this->model->where('estado', $estado)->get();
+    }
+
+    public function getByZona(int $zonaId)
+    {
+        return $this->model->where('zona_id', $zonaId)->get();
+    }
+
+    public function getByEmpleado(int $empleadoId)
+    {
+        return $this->model->where('empleado_id', $empleadoId)->get();
+    }
+}
+
+
