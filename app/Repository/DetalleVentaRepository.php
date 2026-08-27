61f2453afa9a2c1ccb23b0beda249891f8d2fecf
Danna Salcedo <ds0179881@gmail.com>
2026-08-26 21:06:36 -0500

    nuevo cargue

---
+++ app/Repository/DetalleVentaRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\DetalleVentaInterface;
+use App\Models\DetalleVenta;
+
+class DetalleVentaRepository extends BaseRepository implements DetalleVentaInterface
+{
+    public function __construct(DetalleVenta $detalleVenta)
+    {
+        parent::__construct($detalleVenta);
+    }
+
+    public function getByProductoId(int $productoId)
+    {
+        return $this->model->where('producto_id', $productoId)->get();
+    }
+
+    public function getByVentaId(int $ventaId)
+    {
+        return $this->model->where('venta_id', $ventaId)->get();
+    }
+    public function getByCantidad(int $cantidad)
+  {
+     $productos = $this->model->where("cantidad", $cantidad)->get();
+
+        if($productos->empty())
+        {
+            return null;
+        }
+
+        return $productos;
+  }
+}
