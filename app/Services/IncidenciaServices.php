f5fd795631c14f99b3112463f889e468c2c66cce
YessikaJara <yessikajh@gmail.com>
2026-09-01 21:03:50 -0500

    Se crea servicio Incidencias

---
+++ app/Services/IncidenciaServices.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\IncidenciaInterface;
+
+class IncidenciaService
+{
+    public function __construct(
+        private IncidenciaInterface $incidenciaRepository
+    ) {}
+
+    public function getAll()
+    {
+        return $this->incidenciaRepository->getAll();
+    }
+
+    public function getById(int $id)
+    {
+        return $this->incidenciaRepository->getById($id);
+    }
+
+    public function create(array $datos)
+    {
+        return $this->incidenciaRepository->create($datos);
+    }
+
+    public function update(array $datos, int $id)
+    {
+        return $this->incidenciaRepository->update($datos, $id);
+    }
+
+    public function delete(int $id)
+    {
+        return $this->incidenciaRepository->delete($id);
+    }
+
+    public function getByEstado(string $estado)
+    {
+        return $this->incidenciaRepository->getByEstado($estado);
+    }
+
+    public function getByZona(int $zonaId)
+    {
+        return $this->incidenciaRepository->getByZona($zonaId);
+    }
+
+    public function getByEmpleado(int $empleadoId)
+    {
+        return $this->incidenciaRepository->getByEmpleado($empleadoId);
+    }
+}
